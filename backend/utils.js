d6239536cae2c3e47cadf1f5a61f341887359b55
gultar <sacha-olivier.dulac@hotmail.com>
2019-04-11 13:35:12 -0400

    modified logging output

--- backend/utils.js
+++ backend/utils.js
@@ -1,20 +1,23 @@
 const displayTime = () =>{
-  var d = new Date(),   // Convert the passed timestamp to milliseconds
-    year = d.getFullYear(),
-    mnth = d.getMonth(),        // Months are zero based. Add leading 0.
-    day = d.getDay(),                   // Add leading 0.
-    hrs = d.getHours(),
-    min = d.getMinutes(),
-    sec = d.getSeconds(),               // Add leading 0.
-    ampm = 'AM';
+  // var d = new Date(),   // Convert the passed timestamp to milliseconds
+  //   year = d.getFullYear(),
+  //   mnth = d.getMonth(),        // Months are zero based. Add leading 0.
+  //   day = d.getDay(),                   // Add leading 0.
+  //   hrs = d.getHours(),
+  //   min = d.getMinutes(),
+  //   sec = d.getSeconds(),               // Add leading 0.
+  //   ampm = 'AM';
 
-    return hrs+":"+min+":"+sec;
+  //   return hrs+":"+min+":"+sec;
+  let date = new Date();
+  let time = date.toLocaleTimeString();
+  return time;
 }
 
 const logger = (message, arg) => {
-  // let date = new Date();
-  // let time = date.toLocaleTimeString();
-  let beautifulMessage = '['+ displayTime() +'] ' + message;
+  let date = new Date();
+  let time = date.toLocaleTimeString();
+  let beautifulMessage = '['+ time +'] ' + message;
   if(arg){
     console.log(beautifulMessage, arg);
   }else{
