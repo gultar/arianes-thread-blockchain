a3ba25a76d8639f27f9e5c8409ff4a42d70334bc
gultar <sacha-olivier.dulac@hotmail.com>
2020-07-27 07:57:54 -0400

    testing new state storage

--- backup_modules/modules/classes/validating/launchValidator.js
+++
@@ -1,9 +0,0 @@
-const Validator = require('./validator')
-const { workerData } = require('worker_threads')
-let validator = new Validator({
-    publicKey:workerData.publickey,
-    verbose:workerData.verbose,
-    keychain:{ name:workerData.walletName, password:workerData.password },
-    numberOfCores: 1
-})
-validator.connect(workerData.nodeAddress)
--- backup_modules/modules/tools/genesisConfigHash.js
+++
@@ -1,7 +0,0 @@
-const genesis = require('./getGenesis')
-const sha256 = require('./sha256')
-
-module.exports = getGenesisConfigHash = () => {
-    let genesisString = JSON.stringify(genesis)
-    return sha256(genesisString)
-}
