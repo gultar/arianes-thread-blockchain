b6bc79369cc8616288d5fd185d3e4c86f72b4702
gultar <sacha-olivier.dulac@hotmail.com>
2020-08-12 09:18:11 -0400

    Trying to avoid getting useless blocks from miner

--- modules/classes/blockchain/chain.js
+++ modules/classes/blockchain/chain.js
@@ -315,7 +315,7 @@ class Blockchain{
         this.isRoutingBlock = newBlock.blockNumber
         let success = await this.routeBlock(newBlock)
         this.isRoutingBlock = false
-        this.minerChannel('nodeEvent','startMining')
+        global.minerChannel('nodeEvent','startMining')
         return success
       
       
