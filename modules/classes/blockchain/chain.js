cf9452effe495cb6dae54bbb9acd097e8825ef20
gultar <sacha-olivier.dulac@hotmail.com>
2020-08-26 20:14:49 +0000

    kk

--- modules/classes/blockchain/chain.js
+++ modules/classes/blockchain/chain.js
@@ -1745,7 +1745,7 @@ class Blockchain{
           let startExecute = process.hrtime()
           let results = await this.executeManyCalls(calls)
           let endExecute = process.hrtime(startExecute)
-          console.log(`Executed ${Object.keys(calls).length} calls in block in: ${endExecute[1]/1000000}`)
+          
           if(results){
             if(results.error) resolve({error:results.error})
             else if(Object.keys(results).length > 0){
