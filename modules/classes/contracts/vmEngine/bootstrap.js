2bac8f65b8047bc610bc6028173d2f3d97f3aa24
gultar <sacha-olivier.dulac@hotmail.com>
2020-08-26 17:27:34 +0000

    trying to boot VM instead of sending initContract message to worker

--- modules/classes/contracts/vmEngine/bootstrap.js
+++ modules/classes/contracts/vmEngine/bootstrap.js
@@ -111,13 +111,13 @@ class Bootstrap{
             }
         }
 
-        let worker = await this.bootVM({ contractName, contractCode:memory.contract, state:memory.state })
+        let worker = await this.bootVM({ contractName, contractCode:contractCode, state:state })
         /**worker.postMessage({ 
             initContract:{
                 contract:memory.contract, state:memory.state, contractName:contractName,
             } 
         }) */
-        return { sent: true }
+        return { contractIntialized: true }
         
     }
 
